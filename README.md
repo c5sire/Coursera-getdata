79b7ffc01dba8d937206891e1e9c733f1bb4f18e
Reinhard Simon <R.SIMON@CGIAR.ORG>
2014-06-22 07:14:39 -0500

    Added readme.

--- README.md
+++ README.md
@@ -4,5 +4,13 @@ Coursera-getdata
 Coursera-getdata assignment
 -----
 
+The script follows the basic instructions in the course project:
 
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject
+
+Comments inside the script refer to the steps above. There is only one small change in sequence between steps 3 and 4.
 
--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 
 unzip("dataset.zip")
 
-# merge subsets
+# 1. merge subsets
 train = read.table("UCI HAR Dataset/train/X_train.txt", header=F)
 subj  = read.table("UCI HAR Dataset/train/subject_train.txt", header=F)
 actv  = read.table("UCI HAR Dataset/train/y_train.txt", header=F)
@@ -19,10 +19,10 @@ test = cbind(subj, actv, test)
 data  = rbind(train, test)
 rm(train, test, subj, actv)
 
-# filter columns
+# 2. filter columns
 
 colns = read.table("UCI HAR Dataset/features.txt", stringsAs=F)[,2]
-colnames(data) = c("Subject", "Activity", colns)
+colnames(data) = c("Subject", "Activity", colns) # 4. use descriptive variable names
 cols  = str_detect(colns,"mean") | str_detect(colns,"std")
 cols  = colns[cols]
 cols  = c("Subject", "Activity", cols)
@@ -30,7 +30,7 @@ cols  = c("Subject", "Activity", cols)
 data  = data[,cols]
 rm(colns, cols)
 
-# add activity names
+#3.  add activity names
 actv = read.table("UCI HAR Dataset/activity_labels.txt", stringsAsF=F)
 data$Activity = as.character(data$Activity)
 data[data$Activity == "1",2] = actv[1,2]
@@ -44,6 +44,8 @@ rm(actv)
 
 save(data,file="data.Rda")
 data = as.data.frame(data, stringsAsF=F)
+
+# 5. Write tidy data
 
 tidy.data = aggregate(data[,3:ncol(data)], list(Subject=data$Subject, Activity=data$Activity), mean)
 
